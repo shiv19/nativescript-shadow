9381e600a528bd5730780817ddd06fccf51f7cb3
Shiv19 <mailshiva19@gmail.com>
2017-11-03 00:18:08 +0530

    shadows working

--- src/shadow.android.ts
+++ src/shadow.android.ts
@@ -1,8 +1,17 @@
 export class Shadow {
     addShadow(nsView, elevation: number) {
         const nativeView = nsView.android;
+        const parent = nsView.parent.android;
         console.log("came here");
-        nativeView.setElevation(10.0);
-        nativeView.setClipToOutline(false);
+        // nativeView.setClipToOutline(false);
+
+        // parent.setClipToOutline(false);
+        // nativeView.setElevation(10.0);
+
+        var shape = new android.graphics.drawable.GradientDrawable();
+        shape.setShape(android.graphics.drawable.GradientDrawable.RECTANGLE);
+        shape.setColor(android.graphics.Color.parseColor("#ffffff"));
+        nativeView.setBackgroundDrawable(shape);
+        nativeView.setElevation(elevation);
     }
 }
